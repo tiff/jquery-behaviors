984a9530f1bc4369b1293c9d8b5483e064e1213c
Christopher Blum <christopher.blum@xing.com>
2011-07-18 17:10:11 +0200

    First commit

---
+++ behaviors.js
@@ -0,0 +1,206 @@
+/**
+ * Behaviors let you extend elements in an unobtrusive way.
+ * You can add rules by specifying a CSS selector and a corresponding action.
+ * 
+ * Behaviors are automatically assigned as soon as the DOM is ready.
+ * 
+ * @author Christopher Blum (inspired by Martin Kleppe)
+ * 
+ * @example
+ *  // add single behavior via class assignment
+ *  $.behaviors("input[placeholder]", handlePlaceholderFunc);
+ * 
+ *  // add single behavior
+ *  $.behaviors("input.focus", function(input) { input.focus(); });
+ * 
+ *  // add behavior with dom event
+ *  $.behaviors("a.stop:click", function(link, event) { event.preventDefault(); });
+ *  
+ *  // add multiple behaviors
+ *  $.behaviors({
+ *    "input:click, textarea:click": function(element) { element.select(); },
+ *    "input.focus":                 function(input) { input.focus();},
+ *    "a.stop:click":                function(link, event) { event.peventDefault(); }
+ *  });
+ */
+(function($) {
+  var rules                   = {},
+      selectorCache           = {},
+      cleanUp                 = [],
+      $document               = $(document),
+      PSEUDOS                 = $.expr.match.CHILD,
+      REG_EXP_SELECTOR_SPLIT  = /,\s*/;
+  
+  /**
+   * Prepare selector by splitting multiple selectors ("div.bar, span.foo") into
+   * single selectors
+   * @param {String} selector The CSS selector(s).
+   * @param {Function|Class} action Action or class to process element.
+   */
+  function _addRule(selectors, action) {
+    $.each(selectors.split(REG_EXP_SELECTOR_SPLIT), function(i, selector) {
+      _addSingleRule(selector, action);
+    });
+  }
+  
+  /**
+   * Adds a single rule.
+   * @param {String} selector The CSS selector.
+   * @param {Function|Class} action Action or class to process element.
+   */
+  function _addSingleRule(selector, action) {
+    rules[selector] = action;
+    if ($.isReady) {
+      _assign(selector, action, $document);
+    }
+  }
+  
+  /**
+   * Assigns a single rule.
+   * @param {String} selector The CSS selector.
+   * @param {Function|Class} action Action or class to process element.
+   * @param {Element|String} [container] Optional parent element or selector on which the behaviors should applied (It looks inside this container for matches)
+   */
+  function _assign(selector, action, container) {
+    selector = _splitSelector(selector);
+    var allElements;
+    
+    if (selector.event) {
+      if (container.data(selector.original) != action) {
+        var callback = function(event) {
+          action(this, event);
+        };
+        container
+          .data(selector.original, action)
+          .delegate(selector.key, selector.event, callback);
+        
+        cleanUp.push(function() {
+          container.undelegate(selector.key, selector.event, callback);
+        });
+      }
+    } else {
+      if (container) {
+        allElements = $(container).find(selector.key);
+      } else {
+        allElements = $(selector.key);
+      }
+      allElements.each(function(i, element){
+        var $element = $(element);
+        if ($element.data(selector.original) != action) {
+          $element.data(selector.original, action);
+          action(element);
+        }
+      });
+    }
+  }
+  
+  /**
+   * Splits a given selector string into "key" and "event" object.
+   * Used internally to observe events.
+   * 
+   * @param {String} selector The full selector.
+   * 
+   * @returns {Object} An object with "key" and "event properties".
+   * 
+   * @example
+   *  _splitSelector("ul:first-child:hover"); 
+   *    // => { key: "ul:first-child", event: "hover" }
+   */
+  function _splitSelector(selector) {
+    var parts,
+        part,
+        key,
+        cache = selectorCache[selector];
+    
+    if (cache){ 
+      return cache;
+    } else {
+      // split "#id.class:event" into "#id.class" and "event"
+      parts = selector.split(":");
+      key = parts.shift();
+      
+      // reject pseudo classes such as "last-child"
+      while ((part = parts[0]) && PSEUDOS.test(":" + part)) {
+        key += ":" + part;
+        parts.shift();
+      }
+      
+      // return seperated key and event properties
+      return selectorCache[selector] = {
+        key:      key,
+        event:    parts.join(":") || null,
+        original: selector
+      };
+    }
+  }
+  
+  /**
+   * Adds one or more behavior rules.
+   * Hint: You can also pass an event name (eg. "a:click") to observe.
+   * 
+   * @param {String|Object} hashOrSelector An rules has or the CSS selector.
+   * @param {Function|Class} [action] Action or class to process element.
+   * 
+   * @example
+   *  // add single behavior via class assignment
+   *  $.behaviors("input[title]", handlePlaceholderFunc);
+   * 
+   *  // add single behavior
+   *  $.behaviors("input.focus", function(input) { input.focus();});
+   * 
+   *  // add behavior with dom event
+   *  $.behaviors("a.stop:click", function(link, event) { event.preventDefault(); });
+   *  
+   *  // add multiple behaviors
+   *  $.behaviors({
+   *    "input.focus": function(input) { input.focus();},
+   *    "a.stop:click": function(link, event) { event.preventDefault(); },
+   *  });
+   */
+  $.behaviors = function(hashOrSelector, action) {
+    var selector;
+    if (typeof(hashOrSelector) === "string"){
+      _addRule(hashOrSelector, action);
+    } else {
+      for (selector in hashOrSelector) {
+        _addRule(selector, hashOrSelector[selector]);
+      }
+    }
+  };
+  
+  $.clearBehaviors = function() {
+    for (var i=0, length=cleanUp.length; i<length; i++) {
+      cleanUp[i]();
+    }
+    rules   = {};
+    cleanUp = [];
+  };
+  
+  /**
+   * Initializes or updates all rules.
+   * 
+   * @param {String or Object} container This optional param causes, 
+   *  that the update-functionality is narrowd down on the fields inside the container.
+   *
+   * @example
+   *  $("#my_div").html("<span class='hide'>Foo</span>").updateBehaviors();
+   *
+   *  or you can update all behaviors in the entire dom tree
+   *  $(document).updateBehaviors();
+   */
+  $.fn.updateBehaviors = function() {
+    var i, length, selector, $element;
+    for (i=0, length=this.length; i<length; i++) {
+      $element = $(this[i]);
+      for (selector in rules) {
+        _assign(selector, rules[selector], $element);
+      }
+    }
+    return this;
+  };
+  
+  
+  $(function() {
+    $document.updateBehaviors();
+  });
+})(jQuery);
---
+++ test/test.js
@@ -0,0 +1,119 @@
+QUnit.config.reorder = false;
+
+module("$.behaviors", {
+  teardown: function() {
+    $.clearBehaviors();
+  }
+});
+
+
+test("Check adding of simple behaviors", function() {
+  expect(4);
+
+  var anchor1 = $("a.test1")[0],
+      anchor2 = $("a[data-attribute=test-data]")[0];
+  
+  $.behaviors({
+    "a.test1": function(param) {
+      ok(true, "Behavior callback successfully executed");
+      equals(param, anchor1, "Callback passes element as first argument");
+    },
+    "a[data-attribute]": function(param) {
+      ok(true, "Behavior callback successfully executed");
+      equals(param, anchor2, "Callback passes element as first argument");
+    }
+  });
+});
+
+
+test("Check adding of behavior with event", function() {
+  expect(3);
+
+  var input = $("input.test2")[0];
+
+  $.behaviors({
+    "input.test2:click": function(param1, param2) {
+      ok(true, "Behavior callback successfully executed");
+      equals(param1, input, "Callback passes element as first argument");
+      ok(param2.preventDefault && param2.stopPropagation, "Callback passes event object as second argument");
+    }
+  });
+  
+  QUnit.triggerEvent(input, "click");
+});
+
+
+test("Check adding of complex css selector plus complex event", function() {
+  expect(3);
+
+  var $listItems = $("#test-container > ul li");
+
+  $.behaviors({
+    "#test-container > ul li:first-child:custom:event": function(param1, param2) {
+      ok(true, "Behavior callback successfully executed");
+      equals(param1, $listItems[0], "Callback passes element as first argument");
+      ok(param2.preventDefault && param2.stopPropagation, "Callback passes event object as second argument");
+    }
+  });
+  
+  $listItems.trigger("custom:event");
+});
+
+
+test("Check if each behavior is only set once", function() {
+  expect(2);
+  
+  var counter1 = 0,
+      counter2 = 0;
+  
+  $.behaviors({
+    "#test-element":           function() { counter1++; },
+    "#test-element:mousedown": function() { counter2++; }
+  });
+  
+  $(document).updateBehaviors();
+  $(document).updateBehaviors();
+  
+  $("#test-element").trigger("mousedown").trigger("mousedown");
+  
+  equals(counter1, 1);
+  equals(counter2, 2);
+});
+
+
+test("Check if only those behaviors are assigned that are within a particular container", function() {
+  expect(1);
+  
+  $.behaviors({
+    "span.test-element": function() {
+      ok(true);
+    }
+  });
+  
+  var container       = $("<div>"),
+      subElement      = $("<span>", { "class": "test-element" }).appendTo(container),
+      subElementClone = subElement.clone();
+  
+  $("#qunit-fixture").append(container).append(subElementClone);
+  
+  container.updateBehaviors();
+});
+
+
+test("Check multiple selectors with different events", function() {
+  expect(2);
+  
+  var clickElement      = $("div.click-event")[0],
+      mouseOverElement  = $("div.mouseover-event")[0];
+      
+  $.behaviors({
+    ".click-event:click, .mouseover-event:mouseover": function(element) {
+      ok(true, "Fired event on '" +  element.className + "'");
+    }
+  });
+  
+  $(document).updateBehaviors();
+  
+  QUnit.triggerEvent(clickElement, "click");
+  QUnit.triggerEvent(mouseOverElement, "mouseover");
+});
